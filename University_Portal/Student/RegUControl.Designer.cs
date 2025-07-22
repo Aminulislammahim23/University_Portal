4d7ae9ff8bce1db4da4fe7da46f28ad9792e73db
aminulislammahim23 <aminulislammahim2013@gmail.com>
2025-07-23 00:28:04 +0600

    version 7.23.25.1

--- University_Portal/Student/RegUControl.Designer.cs
+++ University_Portal/Student/RegUControl.Designer.cs
@@ -28,18 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbSemester = new System.Windows.Forms.ComboBox();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.pnlRegistered = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.pnlContent.SuspendLayout();
+            this.pnlRegistered.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.cbSemester);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1019, 57);
+            this.panel1.TabIndex = 1;
+            // 
+            // cbSemester
+            // 
+            this.cbSemester.FormattingEnabled = true;
+            this.cbSemester.Location = new System.Drawing.Point(815, 17);
+            this.cbSemester.Name = "cbSemester";
+            this.cbSemester.Size = new System.Drawing.Size(192, 21);
+            this.cbSemester.TabIndex = 1;
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlContent.Controls.Add(this.btnPrint);
+            this.pnlContent.Controls.Add(this.label7);
+            this.pnlContent.Controls.Add(this.label6);
+            this.pnlContent.Controls.Add(this.label5);
+            this.pnlContent.Controls.Add(this.label4);
+            this.pnlContent.Controls.Add(this.label3);
+            this.pnlContent.Controls.Add(this.label2);
+            this.pnlContent.Location = new System.Drawing.Point(13, 77);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(996, 89);
+            this.pnlContent.TabIndex = 2;
+            // 
+            // pnlRegistered
+            // 
+            this.pnlRegistered.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlRegistered.Controls.Add(this.label9);
+            this.pnlRegistered.Controls.Add(this.label8);
+            this.pnlRegistered.Location = new System.Drawing.Point(13, 185);
+            this.pnlRegistered.Name = "pnlRegistered";
+            this.pnlRegistered.Size = new System.Drawing.Size(996, 431);
+            this.pnlRegistered.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lecture:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Studio:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(212, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Science:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Health Science:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(391, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Computer:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(582, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Language:";
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.Red;
+            this.btnPrint.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPrint.Location = new System.Drawing.Point(900, 19);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 6;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Registered";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(430, 19);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Lec-Com-Sci-Lan-Stu-H Sci";
             // 
             // RegUControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlRegistered);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.panel1);
             this.Name = "RegUControl";
             this.Size = new System.Drawing.Size(1025, 646);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlContent.ResumeLayout(false);
+            this.pnlContent.PerformLayout();
+            this.pnlRegistered.ResumeLayout(false);
+            this.pnlRegistered.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbSemester;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Panel pnlRegistered;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
     }
 }
